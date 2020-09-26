1540b7a9e30fdfb6ef4bbb751978c24a52c4b771
qstanley <qastanley25@gmail.com>
2020-09-26 16:28:46 -0500

    Update bestitem.py

--- src/mech/mania/starter_pack/domain/bestitem.py
+++ src/mech/mania/starter_pack/domain/bestitem.py
@@ -1,6 +1,8 @@
 from mech.mania.starter_pack.domain.model.items.shoes import Shoes
 from mech.mania.starter_pack.domain.model.items.weapon import Weapon
 from mech.mania.starter_pack.domain.model.items.clothes import Clothes
+from mech.mania.starter_pack.domain.model.items.hat import Hat
+from mech.mania.starter_pack.domain.model.items.accessory import Accessory
 from mech.mania.starter_pack.domain.model.characters.character_decision import CharacterDecision
 def get_best_item(player, item_type):
     #get inventory
@@ -9,42 +11,55 @@ def get_best_item(player, item_type):
     our_items = [(index, item) for index, item in enumerate(inventory) if type(item) is item_type]
     if (item_type is Weapon):
         curr_weapon = player.get_weapon()
-        curr_pv = curr_weapon.assign_point_value()
+        curr_pv = assign_weapon_point_value(curr_weapon)
         our_items.sort(key=assign_weapon_point_value, reverse=True)
         if (len(our_items) > 0):
             index, best_weapon = our_items[0]
-            best_pv = best_weapon.assign_weapon_point_value()
+            best_pv = assign_weapon_point_value(best_weapon)
             if best_pv > curr_pv:
                 return index
 
     if (item_type is Clothes):
         curr_clothes = player.get_clothes()
-        curr_pv = curr_clothes.assign_clothes_point_value()
+        curr_pv = assign_clothes_point_value(curr_clothes)
         our_items.sort(key=assign_clothes_point_value, reverse=True)
         if (len(our_items) > 0):
             index, best_clothes = our_items[0]
-            best_pv = best_clothes.assign_clothes_point_value()
+            best_pv = assign_clothes_point_value(best_clothes)
             if best_pv > curr_pv:
                 return index
 
     if (item_type is Shoes):
         curr_shoes = player.get_shoes()
-        curr_pv = curr_shoes.assign_shoes_point_value()
+        curr_pv = assign_shoes_point_value(curr_shoes)
         our_items.sort(key=assign_shoes_point_value, reverse=True)
         if (len(our_items) > 0):
             index, best_shoes = our_items[0]
-            best_pv = best_shoes.assign_shoes_point_value()
+            best_pv = assign_shoes_point_value(best_shoes)
+            if best_pv > curr_pv:
+                return index
+
+    if (item_type is Hat):
+        curr_hat = player.get_hat()
+        curr_pv = assign_hat_and_accessory_point_value(curr_hat)
+        our_items.sort(key=assign_hat_and_accessory_point_value, reverse=True)
+        if (len(our_items) > 0):
+            index, best_hat = our_items[0]
+            best_pv = assign_hat_and_accessory_point_value(best_hat)
+            if best_pv > curr_pv:
+                return index
+
+    if (item_type is Accessory):
+        curr_accessory = player.get_accessory()
+        curr_pv = assign_hat_and_accessory_point_value(curr_accessory)
+        our_items.sort(key=assign_hat_and_accessory_point_value, reverse=True)
+        if (len(our_items) > 0):
+            index, best_accessory = our_items[0]
+            best_pv = assign_hat_and_accessory_point_value(best_accessory)
             if best_pv > curr_pv:
                 return index
 
     return -1
-    #for index, item in our_weapons:
-        
-
-        #check if weapon for higher attack
-        # curr_weapon_pv = assign_point_value(curr_weapon)
-        # item_pv = assign_point_value(item)
-        # if type(item) is Weapon and item_pv > curr_weapon_pv:
             
 
 def assign_weapon_point_value(weapon):
@@ -56,6 +71,7 @@ def assign_weapon_point_value(weapon):
         total_pv = total_pv * (1 + weapon.get_splash_radius()-1/10)
     if (weapon.get_range() > 1):
         total_pv += 10
+    total_pv += weapon.get_percent_experience_change * weapon.get_flat_experience_change
     return total_pv
 
 def assign_clothes_point_value(clothes):
@@ -65,8 +81,18 @@ def assign_clothes_point_value(clothes):
     total_pv += clothes.get_flat_attack_change()
     total_pv += clothes.get_flat_regen_per_turn()
     return total_pv
+
 def assign_shoes_point_value(shoes):
 	total_pv = 0
 	total_pv += shoes.get_flat_speed_change()
 	total_pv += shoes.get_percent_speed_change()
 	return total_pv
+
+def assign_hat_and_accessory_point_value(accessory):
+    total_pv = 0
+    total_pv += accessory.get_flat_experience_change() * accessory.get_percent_experience_change()
+    total_pv += accessory.get_flat_defence_change()
+    total_pv += accessory.get_flat_health_change() * accessory.get_percent_health_change()
+    total_pv += accessory.get_flat_attack_change() * accessory.get_percent_attack_change()
+    total_pv += accessory.get_flat_regen_per_turn()
+    return total_pv

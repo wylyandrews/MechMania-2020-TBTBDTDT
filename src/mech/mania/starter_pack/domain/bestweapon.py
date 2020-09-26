0ad679ea5fac876cfae06346f21bad5381d4d3e6
qstanley <qastanley25@gmail.com>
2020-09-26 13:19:57 -0500

    Update bestweapon.py

--- src/mech/mania/starter_pack/domain/bestweapon.py
+++ src/mech/mania/starter_pack/domain/bestweapon.py
@@ -4,14 +4,14 @@ def get_best_weapon(player):
     #get inventory
     inventory = player.inventory
     #get current weapon
-    curr_weapon = player.getWeapon()
+    curr_weapon = player.get_weapon()
     #iterate through inventory
-    for i in inventory:
+    for index, item in enumerate(inventory):
         #check if weapon for higher attack
-        if type(i) is Weapon and i.get_attack() > curr_weapon.get_attack():
+        if type(item) is Weapon and item.get_attack() > curr_weapon.get_attack():
             #equip if better
             return CharacterDecision(
                 decision_type="EQUIP",
                 action_position=None,
-                action_index=player.get_free_inventory_index()
+                action_index=index
             )

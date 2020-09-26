f91c2f558126f0b321ce4031bbb4faf6ec9b3a35
qstanley <qastanley25@gmail.com>
2020-09-26 15:03:31 -0500

    Made best weapon into best item

--- src/mech/mania/starter_pack/domain/bestitem.py
+++ src/mech/mania/starter_pack/domain/bestitem.py
@@ -1,20 +1,20 @@
 from mech.mania.starter_pack.domain.model.items.weapon import Weapon
 from mech.mania.starter_pack.domain.model.characters.character_decision import CharacterDecision
-def get_best_weapon(player):
+def get_best_item(player, item_type):
     #get inventory
     inventory = player.inventory
     #get current weapon
     curr_weapon = player.get_weapon()
     curr_pv = curr_weapon.assign_point_value()
-    #iterate through inventory
-    our_weapons = [(index, item) for index, item in enumerate(inventory) if type(item) is Weapon]
-    our_weapons.sort(key=assign_point_value, reverse=True)
-
-    if (len(our_weapons) > 0):
-        index, best_weapon = our_weapons[0]
-        best_pv = best_weapon.assign_point_value()
-        if best_pv > curr_pv:
-            return index
+    #iterate through weapons
+    our_items = [(index, item) for index, item in enumerate(inventory) if type(item) is item_type]
+    if (item_type is Weapon):
+        our_items.sort(key=assign_weapon_point_value, reverse=True)
+        if (len(our_items) > 0):
+            index, best_weapon = our_items[0]
+            best_pv = best_weapon.assign_weapon_point_value()
+            if best_pv > curr_pv:
+                return index
 
     return -1
     #for index, item in our_weapons:
@@ -26,13 +26,13 @@ def get_best_weapon(player):
         # if type(item) is Weapon and item_pv > curr_weapon_pv:
             
 
-def assign_point_value(weapon):
+def assign_weapon_point_value(weapon):
     total_pv = 0
     total_pv += weapon.get_attack()
     if (weapon.get_on_hit_effect() is not None):
         total_pv = total_pv * (1 + weapon.get_on_hit_effect().get_turns_left()/10)
     if (weapon.get_splash_radius() > 1):
-        total_pv = total_pv * (1 + weapon.get_splash_radius()/10)
+        total_pv = total_pv * (1 + weapon.get_splash_radius()-1/10)
     if (weapon.get_range() > 1):
         total_pv += 10
     return total_pv

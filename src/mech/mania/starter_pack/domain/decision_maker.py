386e8824e84b83a981ced6ac14d95a7de299cef3
sirdude103 <sirdude103@gmail.com>
2020-09-25 22:16:09 -0500

    tracking monsters

--- src/mech/mania/starter_pack/domain/decision_maker.py
+++ src/mech/mania/starter_pack/domain/decision_maker.py
@@ -1,11 +1,13 @@
-
 import mech.mania.starter_pack.domain.helpers as helpers
+from mech.mania.engine.domain.model import character_pb2
 from mech.mania.starter_pack.domain.model.characters.character_decision import CharacterDecision
+
 
 def make_our_decision(api, my_player):
     curr_pos = my_player.get_position()
     enemies = api.find_enemies_by_distance(curr_pos)
-    enemy_pos = enemies[0].get_position()
+    target_enemy = find_ideal_monster(api, my_player)
+    enemy_pos = target_enemy.get_position()
 
     if curr_pos.manhattan_distance(enemy_pos) <= my_player.get_weapon().get_range():
         return CharacterDecision(
@@ -18,3 +20,12 @@ def make_our_decision(api, my_player):
                 decision_type="MOVE",
                 action_position=helpers.find_position_to_move(api, my_player, enemy_pos),
                 action_index=None)
+
+
+def find_ideal_monster(api, my_player):
+    enemies = api.find_enemies_by_distance(my_player.get_position())
+    enemies.sort(key=lambda x: abs(my_player.get_level - x.get_level)) # Enemy closest to my level
+    return enemies[0]
+
+    #if 1 <= my_level <= 2:
+    #    target_monster = 

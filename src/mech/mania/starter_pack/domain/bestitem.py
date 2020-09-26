14631be69b4e0a01d015edecc8f6c43f78cf562a
qstanley <qastanley25@gmail.com>
2020-09-26 15:48:50 -0500

    Update bestitem.py

--- src/mech/mania/starter_pack/domain/bestitem.py
+++ src/mech/mania/starter_pack/domain/bestitem.py
@@ -1,21 +1,29 @@
 from mech.mania.starter_pack.domain.model.items.weapon import Weapon
+from mech.mania.starter_pack.domain.model.items.clothes import Clothes
 from mech.mania.starter_pack.domain.model.characters.character_decision import CharacterDecision
 def get_best_item(player, item_type):
     #get inventory
     inventory = player.inventory
-    #get current weapon
-    curr_weapon = player.get_weapon()
-    curr_pv = curr_weapon.assign_point_value()
-    #iterate through weapons
+    
     our_items = [(index, item) for index, item in enumerate(inventory) if type(item) is item_type]
     if (item_type is Weapon):
+        curr_weapon = player.get_weapon()
+        curr_pv = curr_weapon.assign_point_value()
         our_items.sort(key=assign_weapon_point_value, reverse=True)
         if (len(our_items) > 0):
             index, best_weapon = our_items[0]
             best_pv = best_weapon.assign_weapon_point_value()
             if best_pv > curr_pv:
                 return index
-
+    if (item_type is Clothes):
+        curr_clothes = player.get_clothes()
+        curr_pv = curr_clothes.assign_clothes_point_value
+        our_items.sort(key=assign_clothes_point_value, reverse=True)
+        if (len(our_items) > 0):
+            index, best_clothes = our_items[0]
+            best_pv = best_clothes.assign_clothes_point_value()
+            if best_pv > curr_pv:
+                return index
     return -1
     #for index, item in our_weapons:
         
@@ -28,7 +36,7 @@ def get_best_item(player, item_type):
 
 def assign_weapon_point_value(weapon):
     total_pv = 0
-    total_pv += weapon.get_attack()
+    total_pv += weapon.get_attack() * (.25+ (weapon.get_flat_attack_change() * (1 + weapon.get_percent_attack_change())))
     if (weapon.get_on_hit_effect() is not None):
         total_pv = total_pv * (1 + weapon.get_on_hit_effect().get_turns_left()/10)
     if (weapon.get_splash_radius() > 1):
@@ -36,4 +44,11 @@ def assign_weapon_point_value(weapon):
     if (weapon.get_range() > 1):
         total_pv += 10
     return total_pv
-    
+
+def assign_clothes_point_value(clothes):
+    total_pv = 0
+    total_pv += clothes.get_flat_defence_change() * clothes.get_percent_defence_change()
+    total_pv += clothes.get_flat_health_change() * clothes.get_percent_health_change()
+    total_pv += clothes.get_flat_attack_change()
+    total_pv += clothes.get_flat_regen_per_turn()
+    return total_pv

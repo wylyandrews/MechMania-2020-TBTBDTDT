56b200947308129e475f2e6aec271f6cc0cfa13c
qstanley <qastanley25@gmail.com>
2020-09-26 14:37:46 -0500

    Update bestweapon.py

--- src/mech/mania/starter_pack/domain/bestweapon.py
+++ src/mech/mania/starter_pack/domain/bestweapon.py
@@ -5,18 +5,26 @@ def get_best_weapon(player):
     inventory = player.inventory
     #get current weapon
     curr_weapon = player.get_weapon()
+    curr_pv = curr_weapon.assign_point_value()
     #iterate through inventory
-    for index, item in enumerate(inventory):
+    our_weapons = [(index, item) for index, item in enumerate(inventory) if type(item) is Weapon]
+    our_weapons.sort(key=assign_point_value, reverse=True)
+
+    if (len(our_weapons) > 0):
+        index, best_weapon = our_weapons[0]
+        best_pv = best_weapon.assign_point_value()
+        if best_pv > curr_pv:
+            return index
+
+    return -1
+    #for index, item in our_weapons:
+        
+
         #check if weapon for higher attack
-        curr_weapon_pv = assign_point_value(curr_weapon)
-        item_pv = assign_point_value(item)
-        if type(item) is Weapon and item_pv > curr_weapon_pv:
-            #equip if better
-            return CharacterDecision(
-                decision_type="EQUIP",
-                action_position=None,
-                action_index=index
-            )
+        # curr_weapon_pv = assign_point_value(curr_weapon)
+        # item_pv = assign_point_value(item)
+        # if type(item) is Weapon and item_pv > curr_weapon_pv:
+            
 
 def assign_point_value(weapon):
     total_pv = 0
